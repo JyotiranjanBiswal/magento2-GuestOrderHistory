09040aba86edfa71ec5bda8ea0d35a17556861db
JyotiranjanBiswal <jyotiranjan.biswal87@gmail.com>
2018-10-10 19:28:53 +0530

    Added skipped file

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Born_OrderController',
+    'JRB_OrderController',
     __DIR__
 );
